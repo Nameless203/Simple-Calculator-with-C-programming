ca61de74bc3a6af9737fb0ff7243ac982dc69f62
Nameless203 <43174452+Nameless203@users.noreply.github.com>
2024-08-04 21:43:43 +0800

    A simple Calculator function
    Add core of calculator function
    include add function, sub function, multiply function , divide function and do ....  while function

---
+++ Calculator function.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    char selector;
+    float num1;
+    float num2;
+    float ans;
+    int repeat;
+    do {
+        printf("Write the equation in this form num1 operator num2\n");
+        printf("Please noted that for multiple please use * instead x\n");
+        scanf("%f %c %f",&num1,&selector,&num2);
+        switch (selector) {
+            case '+':
+                ans=num1+num2;
+                break;
+            case '-':
+                ans=num1-num2;
+                break;
+            case '*':
+                ans=num1*num2;
+                break;
+            case '/':
+                ans=num1/num2;
+                break;
+            default:
+                printf("Please try again");
+        }
+        printf("The answer for equation = %f\n",ans);
+        printf("Do you do start with new equation 1 for continue , 0 for no\n");
+        scanf("%d",&repeat);
+
+    } while (repeat == 1);
+}
